a89a1b5fef960d7d881d0c18b48f970b1ef4e7b2
SaadEddinSalila <0959979@hr.nl>
2020-04-05 00:04:34 +0100

    models en contros

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace meldboek.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+        public IActionResult LogIn(string email, string password)
+        {
+           if (email != "")
+            {
+                return View();
+            }
+            email = "saadeddinsalila@gmail.com";
+            password = "1234567";
+            
+            
+            return View();
+            
+        }
+
+    }
+}
---
+++ Models/Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace meldboek.Models
+{
+    public class Admin
+
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+        public Admin(string firstname, string lastname, string email, string password)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            Password = password;
+
+        }
+       
+
+    }
+
+}
