30b327724cc99de68235f2026415540ea1f20109
Amy <0963427@hr.nl>
2020-04-30 16:50:04 +0200

    CSS + Javascript voor topbar en sidenav
    In de root staat nu CSS en Javascript voor de topbar en sidenav, zodat iedereen deze gemakkelijk in de view kan oproepen.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,29 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+/*
+    nav() is triggered by the menubutton (class navbtn)
+    The function opens and closes the sidemenu by changing the width of the overlay
+    It first determines whether the sidemenu is open (20%) or closed (0%)
+    If the sidemenu is closed, it opens it up and the other way around
+*/
+function nav() {
+    if (document.getElementById("nav").style.width == "0%") {
+        document.getElementById("nav").style.width = "20%";
+    }
+    else {
+        document.getElementById("nav").style.width = "0%";
+    }
+}
+
+/*
+    The functions hover and unhover change the account picture when the mouse is hovering on it
+    The difference between the two picture is the background color, so hovering makes it look like
+    the button is 'selected'
+*/
+function hover(element) {
+    element.setAttribute('src', '@Url.Content("~/Content/account_hover2.png")');
+}
+function unhover(element) {
+    element.setAttribute('src', '@Url.Content("~/Content/account.png")');
+}
