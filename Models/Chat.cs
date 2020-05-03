3497355ecec818bc58a117bf2cfd0916f124fba8
Jari <personal@jariketting.com>
2020-05-03 12:36:11 +0200

    Add chat and message model

---
+++ Models/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace meldboek.Models
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace meldboek.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public String Content { get; set; }
+        public DateTime DatetimeSend { get; set; }
+        public DateTime DatetimeRead { get; set; }
+    }
+}
