687e5560919b57cdc74147c985af924eb758dc4f
Jari <personal@jariketting.com>
2020-05-03 17:06:28 +0200

    Finish chat

--- Models/Message.cs
+++ Models/Message.cs
@@ -8,8 +8,9 @@ namespace meldboek.Models
     public class Message
     {
         public int MessageId { get; set; } // Id of message 
+        public string Username { get; set; } // username of user send message
         public String Content { get; set; } // content in text format
         public DateTime DatetimeSend { get; set; } // datetime message was send
-        public DateTime DatetimeRead { get; set; } // datetime message was read (only used in person to person comminucation)
+        public DateTime? DatetimeRead { get; set; } // datetime message was read (only used in person to person comminucation)
     }
 }
