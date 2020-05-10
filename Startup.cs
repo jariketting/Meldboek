f1c05536bb924e2254ed113fa1f5d1e809a71064
Jurrilicious <jurriaanvanderzwaag@hotmail.com>
2020-05-10 14:30:39 +0200

    Revert "Merge remote-tracking branch 'origin/Amy' into Jurriaan"
    This reverts commit 5fae60e3810b4439c2f6767296123333329687e8, reversing
    changes made to bda15674a528f45fc07ad2b56d4f4309f6bb6fdb.

--- Startup.cs
+++ Startup.cs
@@ -57,7 +57,7 @@ namespace meldboek
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Person}/{action=Home}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
