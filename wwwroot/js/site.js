f691a4e198a83e7d70f332b5588ed4a482e3c8d1
Amy <0963427@hr.nl>
2020-05-13 16:02:00 +0200

    site.js fix na merge + Neo4j poort aanpassen
    site.js was opeens leeg en Neo4j denkt altijd dat de standaard port bezet is rip

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*
+    nav() is triggered by the menubutton (class navbtn)
+    The function opens and closes the sidemenu by changing the width of the overlay
+    It first determines whether the sidemenu is open (20%) or closed (0%)
+    If the sidemenu is closed, it opens it up and the other way around
+*/
+function nav() {
+    if (document.getElementById("nav").style.width == "0%") {
+        document.getElementById("nav").style.width = "20%";
+    }
+    else {
+        document.getElementById("nav").style.width = "0%";
+    }
+}
